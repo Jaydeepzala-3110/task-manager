3e4f54f801ef9160deabc96126fc51370021882a
Jaydeepzala-3110 <zalajaydeep3110@gmail.com>
2025-08-28 19:31:11 +0530

    feat : Added response util file

---
+++ server/src/utils/response.utils.ts
@@ -0,0 +1,85 @@
+interface IResponse {
+    code: number;
+    success: boolean;
+    message: any;
+    data: any;
+    err: any;
+  }
+  
+  let responses: IResponse = {
+    code: 200,
+    success: true,
+    message: null,
+    data: null,
+    err: null,
+  };
+  
+  function successResponse(res: any, message: string, data = {}) {
+    let response = responses;
+    response.code = 200;
+    response.success = true;
+    response.message = message;
+    response.data = data;
+    response.err = null;
+    return res.status(response.code).send(response);
+  }
+  
+  function badRequestResponse(res: any, message: string) {
+    let response = responses;
+    response.code = 400;
+    response.success = false;
+    response.message = message;
+    response.data = null;
+    response.err = message;
+    return res.status(response.code).send(response);
+  }
+  
+  function unauthorizedResponse(res: any, message: string) {
+    let response = responses;
+    response.code = 401;
+    response.success = false;
+    response.message = message;
+    response.data = null;
+    response.err = message;
+    return res.status(response.code).send(response);
+  }
+  
+  function notFoundResponse(res: any, message: string) {
+    let response = responses;
+    response.code = 404;
+    response.success = false;
+    response.message = message;
+    response.data = null;
+    response.err = message;
+    return res.status(response.code).send(response);
+  }
+  
+  function internalServerErrorResponse(res: any, message: string, err: any) {
+    let response = responses;
+    response.code = 500;
+    response.success = false;
+    response.message = message;
+    response.data = null;
+    response.err = err;
+    return res.status(response.code).send(response);
+  }
+  
+  function validationFailResponse(res: any, message: string, err: any) {
+    let response = responses;
+    response.code = 422;
+    response.success = false;
+    response.message = message;
+    response.data = null;
+    response.err = err;
+    return res.status(response.code).send(response);
+  }
+  
+  export {
+    successResponse,
+    badRequestResponse,
+    notFoundResponse,
+    unauthorizedResponse,
+    internalServerErrorResponse,
+    validationFailResponse,
+  };
+  
